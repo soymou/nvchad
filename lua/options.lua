0e1d363758184c8d352441973ee2ff4bc5745eb9
soymou <emilio.junoy@gmail.com>
2025-10-17 18:49:04 -0600

    Spell

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,15 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
+
+-- Spell checking
+vim.opt.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
+vim.opt.spellsuggest = "best,9"
+o.spell = true
+o.spelllang = "en_us,es_mx"
+
+-- Add config spell directory to runtime path
+vim.opt.rtp:append(vim.fn.stdpath("config"))
+
 -- o.cursorlineopt ='both' -- to enable cursorline!
