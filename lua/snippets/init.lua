3755260938e59f265c1b054f9f4bc1f6ddd49cca
soymou <emilio.junoy@gmail.com>
2025-10-17 18:17:46 -0600

    Sniop

---
+++ lua/snippets/init.lua
@@ -0,0 +1,19 @@
+-- Main snippets loader
+-- This file loads all filetype-specific snippet files
+
+local ls = require("luasnip")
+
+-- Set up LuaSnip configuration
+ls.config.set_config({
+  history = true,
+  updateevents = "TextChanged,TextChangedI",
+  enable_autosnippets = true,
+})
+
+-- Load filetype-specific snippets
+require("snippets.mxd")
+
+-- You can add more snippet files here as you create them
+-- require("snippets.typescript")
+-- require("snippets.rust")
+-- etc.
---
+++ lua/snippets/mdx.lua
@@ -0,0 +1,34 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local l = ls.text_node
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+ls.add_snippets("mdx", {
+--- Code
+  s("code", fmt([[
+```code :language {} :eval {} 
+{} 
+
+```
+  ]], {
+      i(1, "python"),
+      i(2, "false"),
+      i(3),
+      i(0)
+    })),
+
+--- Aside 
+  s("aside", fmt([[
+<div id="{}">
+<Aside type="{}" title="{}">
+{}
+</Aside>
+</div>
+  ]], {
+      i(1),
+      i(2, "tip"),
+      i(3),
+      i(4)
+    })),
+})
