1cec4b974c258c22f3ff0d074e2b92534d1eed10
soymou <emilio.junoy@gmail.com>
2025-10-17 23:10:02 -0600

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,13 +16,4 @@ return {
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
 }
